092986f22735d664ea1668f396f60732500d8227
pedro <pedro@DESKTOP-LTBU9HU>
2023-02-26 20:16:25 +0100

    Mensaje de Pedro José

---
+++  github_raul_pedrojose/src/github_raul_pedrojose/Github_raul_pedrojose.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github_raul_pedrojose;
+
+/**
+ *
+ * @author pedro
+ */
+public class Github_raul_pedrojose {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Modificación hecha por Pedro José
+        System.out.print("Soy Pedro José");
+        
+    }
+    
+}
